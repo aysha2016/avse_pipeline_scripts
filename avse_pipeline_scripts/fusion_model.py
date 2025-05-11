571008fb594be0c990f22a0dd281c5b424d7d376
Aysha munawwara   Rafik <amunawwara@gmail.com>
2025-05-11 17:01:20 +0530

    Delete avse_pipeline_scripts directory

--- avse_pipeline_scripts/fusion_model.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def fuse_audio_visual_meg(audio_features, visual_features, attention_flag):
-    if attention_flag:
-        fusion = (audio_features + np.mean(visual_features)) * 0.5
-    else:
-        fusion = audio_features * 0.3
-    return fusion
--- avse_pipeline_scripts/meg_processing.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from scipy.signal import spectrogram
-
-def process_meg_signal(meg_data):
-    f, t, Sxx = spectrogram(meg_data)
-    attention_score = np.mean(Sxx)
-    return attention_score > np.median(Sxx)
